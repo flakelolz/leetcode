236a554adcb19907dbd72b941647bbd4c2784cb5
Michael Gonzalez <michael_gonzalez03@hotmail.com>
2023-11-26 07:39:22 -0400

    python two_sum hashmap. Time: O(n) Space: O(n)

--- lc_python/two_sum_test.py
+++ lc_python/two_sum_test.py
@@ -9,11 +9,22 @@ class Solution:
                     return [i, j]
         return []
 
+    def twoSum_hashmap(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}  # key: number, value: index
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in hashmap:
+                return [hashmap[diff], i]
+            hashmap[n] = i
+        return []
+
 
 def test_twoSum_1():
     solution = Solution()
     nums = [2, 7, 11, 15]
     target = 9
+
+    assert solution.twoSum_hashmap(nums, target) == [0, 1]
     assert solution.twoSum_force(nums, target) == [0, 1]
 
 
@@ -21,6 +32,8 @@ def test_twoSum_2():
     solution = Solution()
     nums = [3, 2, 4]
     target = 6
+
+    assert solution.twoSum_hashmap(nums, target) == [1, 2]
     assert solution.twoSum_force(nums, target) == [1, 2]
 
 
@@ -28,5 +41,7 @@ def test_twoSum_3():
     solution = Solution()
     nums = [3, 3]
     target = 6
+
+    assert solution.twoSum_hashmap(nums, target) == [0, 1]
     assert solution.twoSum_force(nums, target) == [0, 1]
 
